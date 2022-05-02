9073953d88041fb55543e1f1c7bf46062faf4e15
IliaBadz <badzaghua.ilia@gmail.com>
2022-05-03 00:01:16 +0400

    Refactor: use trained piplines as python package

--- services/api/spacy_models.py
+++ services/api/spacy_models.py
@@ -3,9 +3,8 @@ import spacy
 
 def load_models() -> dict:
     models = {
-        "en_sm": spacy.load('en_core_web_sm'),
-        "fr_sm": spacy.load("fr_core_news_sm"),
+        'en_sm': spacy.load('en_core_web_sm'),
+        'fr_sm': spacy.load('fr_core_news_sm'),
     }
 
-    print("models loaded from disk")
     return models
